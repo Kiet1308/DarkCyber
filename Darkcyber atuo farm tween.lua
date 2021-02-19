1c30af31ac5115b18fb03db87900a7368d4a0290
Kiet1308 <50774381+Kiet1308@users.noreply.github.com>
2021-02-19 14:44:20 +0700

    Update Darkcyber atuo farm tween.lua

--- Darkcyber atuo farm tween.lua
+++ Darkcyber atuo farm tween.lua
@@ -313,7 +313,13 @@ for k,v in pairs(game.Workspace.Collectibles:GetChildren()) do
       if ((v.Position-sangzboi.p).magnitude <= 60) and (sanghuman.Position-v.Position).magnitude<=60 then
             if automob then
             --rootpart.CFrame = CFrame.new(v.Position.x, sanghuman.Position.y, v.Position.z)
-            tp(CFrame.new(v.Position.x, sanghuman.Position.y, v.Position.z),0.2)
+            if TypeFarming=="Tp" then
+                  sanghuman.CFrame = CFrame.new(v.Position.x, v.Position.Y, v.Position.z)
+                  wait(.1)
+            else
+                  tp(CFrame.new(v.Position.x, sanghuman.Position.y, v.Position.z),times)
+
+            end
             wait()
             end
       end
